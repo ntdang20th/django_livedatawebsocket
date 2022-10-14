15efd70382ba3e9d2c83e75b1ab8edaf1ee82f51
Dang <ntdang_20th@student.agu.edu.vn>
2022-10-14 14:41:05 +0700

    change allow host

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-66p*nn!m755wl3e28!&*^&6#cfi38yugig)u9c)16+@e_=@(a4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','livedatawebsocket.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'livedatawebsocket.herokuapp.com/']
 
 
 # Application definition
@@ -75,10 +75,10 @@ TEMPLATES = [
 ASGI_APPLICATION = 'core.asgi.application'
 
 CHANNEL_LAYERS = {
-            "default": {
+        "default": {
          "BACKEND": "channels.layers.InMemoryChannelLayer",
      },
-    }
+}
 
 
 # Database
@@ -125,8 +125,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
